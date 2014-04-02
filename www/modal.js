33fd089d58d04e7d11612368193aca0bc90059ba
ilyoung <ilyoung@ilyoungui-mbp.purple.lan>
2014-04-02 16:45:52 +0900

    update modal.js

--- www/modal.js
+++ www/modal.js
@@ -1,14 +1,22 @@
 var exec = require('cordova/exec');
 
-function Modal() {
-    this.name = 'Modal';
-}
-
-Modal.prototype = {
+var modal = {
   open: function(url) {
-      exec(null,null,'Modal','open',[url]);
+    exec(null,null,'Modal','open',[url]);
   },
-  close: functoin() {
-      exec(null,null,'Modal','close',[]);
+  close: function() {
+    exec(null,null,'Modal','close',[]);
   }
 }
+// function Modal() {
+//     this.name = 'Modal';
+// }
+//
+// Modal.prototype = {
+//   open: function(url) {
+//       exec(null,null,'Modal','open',[url]);
+//   },
+//   close: functoin() {
+//       exec(null,null,'Modal','close',[]);
+//   }
+// }
