f2525f47497ba137534385074c18c3200e7a2844
namwoong <namwoong.baek@purpleworks.co.kr>
2014-04-02 16:03:58 +0900

    add

--- www/modal.js
+++ www/modal.js
@@ -1,1 +1,14 @@
 var exec = require('cordova/exec');
+
+function Modal() {
+    this.name = 'Modal';
+}
+
+Modal.prototype = {
+  open: function(url) {
+      exec(null,null,'Modal','open',[url]);
+  },
+  close: functoin() {
+      exec(null,null,'Modal','close',[]);
+  }
+}
