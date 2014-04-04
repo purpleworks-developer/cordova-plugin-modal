474b31f64ce5cdf75babf8e385e525d2d50836d0
ilyoung <ilyoung@ilyoungui-mbp.purple.lan>
2014-04-04 14:47:21 +0900

    update modal.jsp and Modal.java

--- src/android/Modal.java
+++ src/android/Modal.java
@@ -4,6 +4,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -11,9 +12,9 @@ import android.content.Intent;
 public class Modal extends CordovaPlugin {
 	private static final int ACTIVITY_MODAL = 1001;							// modal activity request code
 	public static final String PARAM_LOAD_URL = "loadUrl";					// passing data
-	
+
 	private CallbackContext callbackContext;
-	
+
 	@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		this.callbackContext = callbackContext;
@@ -25,6 +26,11 @@ public class Modal extends CordovaPlugin {
             return true;
         } else if(action.equals("close")) {
         	if (cordova.getActivity() instanceof ModalActivity) {
+        		JSONObject jsonObject = args.getJSONObject(0);
+        		Intent intent = new Intent();
+        		intent.putExtra("jsonString", jsonObject.toString());
+
+        		this.cordova.getActivity().setResult(Activity.RESULT_OK);
         		this.cordova.getActivity().finish();
 			} else {
 				callbackContext.error("Illegal Argument Exception");
@@ -33,11 +39,18 @@ public class Modal extends CordovaPlugin {
         }
         return false;
     }
-	
+
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		if(requestCode == ACTIVITY_MODAL) {
 			if(resultCode == Activity.RESULT_OK) {
-				this.callbackContext.success("close!!");
+				String jsonString = intent.getStringExtra("jsonString");
+				JSONObject jsonObject = null;
+				try {
+					jsonObject = new JSONObject(jsonString);
+					this.callbackContext.success(jsonObject);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
--- www/modal.js
+++ www/modal.js
@@ -2,10 +2,10 @@ var exec = require('cordova/exec');
 
 var modal = {
   open: function(successCallback, errorCallback, url) {
-    exec(successCallback, errorCallback, 'Modal','open',[url]);
+    exec(successCallback, errorCallback, 'Modal','open', [ url ]);
   },
-  close: function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, 'Modal', 'close',[]);
+  close: function(successCallback, errorCallback, jsonData) {
+    exec(successCallback, errorCallback, 'Modal', 'close', [ jsonData ]);
   }
 }
 
